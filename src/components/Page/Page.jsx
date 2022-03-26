154c37fde5b49ff40644f98468d650ffec339929
anis brahmi <anisbrahmi840@gmail.com>
2022-03-26 23:51:49 +0100

    BASE SETUP FOR OUR APP

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,11 +1,27 @@
 import React, { Fragment } from 'react';
 
-import Header from '../Header';
+import Form from '../Form'
+import Error from '../Error'
+import Loader from '../Loader'
+import Forecast from "../Forecast";
+import useForecast from "../../useForecast";
 
+import Header from '../Header';
+import styles from './Page.module.css'
 const Page = () => {
+    const {isError, isLoading, forecast, submitRequest} = useForecast()
+    const submitSearch = (value) => {
+        submitRequest(value)
+    }
     return (
         <Fragment>
             <Header />
+            <div className={styles.box}>
+                {!isLoading && <Form submitSearch={submitSearch}/> }
+                {isError && <Error message={isError} /> }
+                {isLoading && <Loader /> }
+            </div>
+            {forecast && <Forecast /> }
         </Fragment>
     );
 };
